b8d8c00a48d77d1dcdf9eec7775cba20112cbf8c
Swathisathyan <82638675+Swathisathyan@users.noreply.github.com>
2021-06-09 21:16:40 +0530

    Create product.cc
    this is a program in c++ lanuage asking to provide 2 input numbers to find their product

---
+++ product.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a,b; float p; 
+    cout<<"enter two numbers whose product is to be found:" ;
+    cin >> a;
+    cin >> b;
+   
+    cout<<"The product of the number is :"<< (a*b);
+    return 0;
+}
